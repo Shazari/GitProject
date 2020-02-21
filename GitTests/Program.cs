0f5d43f5bff30583bf508d339c5d3d08013f5030
Shazari <reza.sianid@gmail.com>
2020-02-21 10:54:46 +0100

    Update Program.cs
    changes from Github

--- GitTests/Program.cs
+++ GitTests/Program.cs
@@ -10,7 +10,7 @@ namespace GitTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello leute!");
         }
     }
 }
