632d92343acb25a84de72e998336655ce55f2aa0
Reza Shazari <R.Shazari@deskcenter.com>
2020-02-21 15:08:39 +0100

    gfhgfh

--- GitTests/Program.cs
+++ GitTests/Program.cs
@@ -12,7 +12,6 @@ namespace GitTests
         {
             Console.WriteLine("Hello World!!!!");
             Console.WriteLine("Hello World");
-            Console.WriteLine("Hello !!!!");
         }
     }
 }
