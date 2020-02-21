5592d3a48caaba3cf97e622f6097c818fc61b860
Reza Shazari <R.Shazari@deskcenter.com>
2020-02-21 15:11:24 +0100

    Commited2

--- GitTests/Program.cs
+++ GitTests/Program.cs
@@ -14,6 +14,9 @@ namespace GitTests
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
 
         }
     }
